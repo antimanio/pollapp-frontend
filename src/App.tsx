dfcfe06347b8477078cfa64eecb4cd15fa1161f5
thtroy <89257822+thtroy@users.noreply.github.com>
2025-10-29 11:19:08 +0100

    Saves token as cookie

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Login from './components/login'
 import Main from './components/main'
 import { jwtDecode } from 'jwt-decode'
+import { useCookies } from 'react-cookie'
 
 function App() {
-  const [token, setToken] = useState(null)
+  const [token, setToken] = useState(null);
+
+  const [cookies, setCookie, removeCookie] = useCookies(['auth']);
+
+  useEffect(() => {
+    if(token != cookies?.auth && cookies?.auth != null && token == null) setToken(cookies?.auth);
+  },[cookies]);
+
+  useEffect(() => { 
+    if(token != null) {
+      if(token != cookies?.auth) setCookie("auth", token);
+      if(cookies?.auth != null && token != cookies?.auth) {
+        setToken(cookies?.auth)
+      }
+
+    }
+  },[token]);
 
   return (
     <>
--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,12 @@ import './index.css'
 import App from './App.tsx'
 import CreatePoll from './components/createpoll.tsx'
 import Login from './components/login.tsx'
+import { CookiesProvider } from 'react-cookie'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <CookiesProvider defaultSetOptions={{ path: '/' }}>
       <App/>
+    </CookiesProvider>
   </StrictMode>,
 )
